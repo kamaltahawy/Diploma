69fbadfd7d9054bb659179a4da1d9190f52bb5eb
kamal <kamaleltahawyy@gmail.com>
2023-12-08 23:37:12 +0200

    Add unit-2(lec.4 lab)

---
+++ Unit-2/lecture 4/lab/main.c
@@ -0,0 +1,26 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 8, 2023
+ *      Author: kamal
+ */
+
+//Store and Print 10 Students Degrees
+#include "stdio.h"
+int main()
+{
+	int x;
+	float degrees[10];
+
+	for(x=0;x<=9;x++)
+	{
+		printf("Enter student %d degree: ",x+1);
+		fflush(stdin);  fflush(stdout);
+		scanf("%f",&degrees[x]);
+	}
+	for(x=0;x<=9;x++)
+	{
+		printf("\n Student %d degree is %f ",x+1,degrees[x]);
+	}
+	return 0;
+}
